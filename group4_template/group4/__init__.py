2932f1881d93762a41fd4e5c196f0c026089c9a4
amink0 <s0kramin@uni-bonn.de>
2022-02-06 11:53:19 +0100

    package template

---
+++ group4_template/group4/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for group4."""
+
+__author__ = """Group 4"""
+__email__ = 'kritiamin@gmail.com'
+__version__ = '0.1.0'
---
+++ group4_template/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+requirements = [ ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Group 4",
+    author_email='kritiamin6461@qmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    description="Group Project Package",
+    entry_points={
+        'console_scripts': [
+            'group4=group4.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    include_package_data=True,
+    keywords='group4',
+    name='group4',
+    packages=find_packages(include=['group4', 'group4.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    version='0.1.0',
+    zip_safe=False,
+)
