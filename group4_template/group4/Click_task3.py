593b9cdb4a562e8074add59729f7d1c153829625
xerxae0 <elena.xerxa@gmail.com>
2022-02-06 14:16:14 +0100

    typos corrections Click_task3.py

--- group4_template/group4/Click_task3.py
+++ group4_template/group4/Click_task3.py
@@ -1,21 +1,25 @@
 import click
-from utils import *  # names imported: URL, PUT_Request, GET_Request, Program, Database_PDB, RID, GET_query_head, url_request_head
-from Blast import Blast_orfs, check_request_status, Blast_sequence
+from group4_template.group4.Blast import Blast_orfs
+
+
 # ok
 @click.group()
 def cli():
     pass
 
+
 @click.command()
 @click.argument('orflist')  # NB This should be provided by Faiza code
 @click.option('-k', '--keep_files', default=True, help="Decide if to store the file results or erase")
 @click.option('-f', '--filename', default="temporary", help="Give a file name where the results will be saved.")
-@click.option('-t', '--file_type', default="html", help="Decide in which format to save the results.Allowed format are html or zip")
+@click.option('-t', '--file_type', default="html",
+              help="Decide in which format to save the results.Allowed format are html or zip")
 def predict(orflist, filename, file_type, keep_files):
-    list_results = Blast_sequence (orflist, filename, file_type, keep_files)
-    print (list_results)
+    list_results = Blast_orfs(orflist, filename, file_type, keep_files)
+    print(list_results)
 
-cli.add_command(predict) 
+
+cli.add_command(predict)
 
 if __name__ == '__main__':
-    cli() 
+    cli()
