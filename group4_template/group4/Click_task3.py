1d357000e673b7b7e6b188698e03432e4f7266bd
amink0 <s0kramin@uni-bonn.de>
2022-02-06 12:39:30 +0100

    package update

--- group4_template/group4/Click_task3.py
+++ group4_template/group4/Click_task3.py
@@ -8,12 +8,11 @@ def cli():
 
 @click.command()
 @click.argument('orflist')  # NB This should be provided by Faiza code
-@click.argument('filename')
 @click.option('-k', '--keep_files', default=True, help="Decide if to store the file results or erase")
 @click.option('-f', '--filename', default="temporary", help="Give a file name where the results will be saved.")
 @click.option('-t', '--file_type', default="html", help="Decide in which format to save the results.Allowed format are html or zip")
-def predict(orflist, filename):
-    list_results = Blast_sequence (orf, filename, file_type, keep_files)
+def predict(orflist, filename, file_type, keep_files):
+    list_results = Blast_sequence (orflist, filename, file_type, keep_files)
     print (list_results)
 
 cli.add_command(predict) 
